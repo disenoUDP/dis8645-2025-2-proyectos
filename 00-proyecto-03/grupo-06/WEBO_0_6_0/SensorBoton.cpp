7301e0d5764829507637fddb9c02d7ce47690a28
SebastianSaez1003 <sebastian.saez1@mail.udp.cl>
2025-11-21 07:12:43 -0300

    Add files via upload

---
+++ 00-proyecto-03/grupo-06/WEBO_0_6_0/SensorBoton.cpp
@@ -0,0 +1,38 @@
+#include "SensorBoton.h"
+
+SensorBoton::SensorBoton(){}
+
+void SensorBoton::configuracionBoton() {
+  // establecer cual es el pint que estara conectado el boton
+  // sera un input, osea que recibira el paso de señal
+  pinMode(botonPin, INPUT);
+  Serial.begin(9600);
+}
+
+// funcionamiento del boton
+void SensorBoton::funcionaBoton() {
+
+// una variable llamada estadoBoton
+// es equivalente al dato que es recibido por la
+// lectura de señal del pint establecido para el boton
+  estadoBoton = digitalRead(botonPin); 
+
+// si es que el estado del boton esta activado
+// osea, recibe señal
+// este se puede mantener presionado, aumentando constantemente
+// lo cual no aporta para la frustacion de la maquina, corregir en el futuro
+  if (estadoBoton == HIGH){
+    // los segundos van a aumentar en 5
+    segundos +=5;
+  }
+
+// los segundos disminuiran en 1
+  segundos -= 1;
+  // enviar al monitor Serial la cantidad de segundos actuales
+  Serial.println(segundos);
+  // enviar al monitor Serial si es que el boton esta siendo presionado
+  Serial.println(estadoBoton);
+  // cada 1 segundo, para hacer que (segundos -= 1) sea similar al paso de tiempo
+  delay(1000);
+
+}
---
+++ 00-proyecto-03/grupo-06/WEBO_0_6_0/SensorBoton.h
@@ -0,0 +1,34 @@
+#ifndef SENSOR_BOTON_H
+#define SENSOR_BOTON_H
+
+// INCLUIR BIBLIOTECAS DE USO PARA SENSOR/ACTUADOR
+ #include <arduino.h>
+
+// CREAR LA CLASE DE NUESTRO SENSOR/ACTUADOR
+  class SensorBoton {
+  public:
+  SensorBoton();
+// especificar todos los voids que seran utilizados en el archivo.cpp
+  void configuracionBoton();
+  void funcionaBoton();
+
+// si es que se necesitas especificar variables, deberian ir aqui
+// ya sean bool int, ect.
+
+  // escribir una booleana para representar si el boton esta prendido o no
+  bool estadoBoton = false;
+
+  // el pin 11 del arduino es donde sera recibida la señal
+  int botonPin = 11;
+
+  // establecer la cantidad de segundos iniciales
+  int segundos = 15;
+
+  // establecer la cantidad de minutos iniciales, que no estan siendo ocupados por ahora
+  int minutos = 0;
+
+
+
+
+};
+#endif
