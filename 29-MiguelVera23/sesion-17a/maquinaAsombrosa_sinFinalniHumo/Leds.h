7d988970ccc17e64898ca08f875fffafdcf51de0
MiguelVera23 <miguel.vera@mail.udp.cl>
2025-12-02 12:27:13 -0300

    Add files via upload

---
+++ 29-MiguelVera23/sesion-17a/maquinaAsombrosa_sinFinalniHumo/Leds.h
@@ -0,0 +1,25 @@
+//base de funcionamiento para clases
+#ifndef LEDS_H
+#define LEDS_H
+#include "Arduino.h"
+//declara una vez la clase Leds
+class Leds {
+  public:
+//declara la existencia de la clase leds
+  Leds();
+//declara los voids que hacen funcionar los componentes en el principal
+void prepararLeds();
+  void usarLeds();
+
+// Entradas de los leds
+int led1 = 4;
+int led2 = 5;
+int led3 = 6;
+int led4 = 7;
+int led5 = 8;
+int nivelLuz = 0;
+
+
+};
+//termina la definición de leds
+#endif 
