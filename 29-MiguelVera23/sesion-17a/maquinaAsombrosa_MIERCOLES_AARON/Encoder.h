6a7ffb99b2b9008bcd519512d07d695284b2df58
aarón montoya-moraga <montoyamoraga@gmail.com>
2025-12-03 18:55:07 -0300

    subir archivos aaron

---
+++ 29-MiguelVera23/sesion-17a/maquinaAsombrosa_MIERCOLES_AARON/Encoder.h
@@ -0,0 +1,41 @@
+#ifndef ENCODER_H
+#define ENCODER_H
+
+#include "Arduino.h"
+
+class Encoder {
+public:
+  Encoder();
+  //establece las funciones que van a realizar los componentes
+  void preparar();
+  void leer();
+
+  // variables de la  manivela
+  int step = 0;
+  int currentStateCLK;
+  int lastStateCLK;
+  int vueltas;
+  int rango;
+  String currentDir = "";
+  unsigned long lastButtonPress = 0;
+  bool giroReproducirAudio = false;
+  int vueltaAnterior = 0;
+  int vueltaActual = 0;
+
+
+  // establecer donde estara conectado cada pin
+  int CLK = 9;
+  int DT = 10;
+  int SW = 11;
+
+  //para que sepa cuanto tiempo ha pasado desde la última comparación de tiempo
+  int tiempoActualEncoder = 0;
+  //la cantidad de tiempo que ha estado prendido el arduino
+  int tiempoNuevoEncoder = millis();
+  //cuanto tiempo quiero que se pause
+  int cantidadDeTiempo = 5000;
+
+  int reiniciarTodo = 0;
+};
+
+#endif
